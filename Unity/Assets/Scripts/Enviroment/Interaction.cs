0f2cb6b80160f6c263fae15cbc61c4d3f0af6e5b
Bonnie Jenkins <18016a@totton.ac.uk>
2022-11-09 13:58:03 +0000

    Interaction Started
    Can now:
    Interact via holding
    Interact via tapping
    Choose the text that shows up
    Stops you from moving when interacting

--- Unity/Assets/Scripts/Enviroment/Interaction.cs
+++ Unity/Assets/Scripts/Enviroment/Interaction.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Player;
 
 namespace Enviroment
 {
@@ -19,15 +21,58 @@ namespace Enviroment
         public GameObject player;
         [Tooltip("How far you can be from the obj")]
         public float dist;
+        [Tooltip("Text object for interaction text")]
+        public TMP_Text textOBJ;
+        [Tooltip("String for input text")]
+        public string text;
+
         float _time;
+        bool holding;
+
+        private void Start()
+        {
+            textOBJ.gameObject.transform.name = string.Format("{0}'s text OBJ", gameObject.transform.name);
+        }
 
         private void Update()
         {
             float distance = Vector3.Distance(player.transform.position, transform.position);
             if(distance < dist)
             {
-                Debug.Log("In Reach");
+                textOBJ.enabled = true;
+                textOBJ.text = text;
+                if(hold) { holding = Input.GetKey(key); }
+                else if(Input.GetKeyDown(key))
+                {
+                    Finish();
+                }
+            } else 
+            {
+                textOBJ.enabled = false;
             }
+
+            if(holding)
+            {
+                player.GetComponent<Movement>().canMove = !holding;
+            }
+
+            TimeManagement();
+        }
+
+        private void TimeManagement()
+        {
+            if(_time < time && !holding) { _time = 0; }
+            if(_time > time) { Finish(); }
+
+            _time += 1 * Time.deltaTime;
+        }
+
+        public void Finish()
+        {
+            Debug.Log("Succesfully Finished!");
+            this.enabled = false;
+            Destroy(textOBJ.gameObject);
+            _time = 0;
         }
     }
 }
