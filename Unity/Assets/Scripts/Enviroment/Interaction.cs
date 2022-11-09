a38f253ca69a9528d45ac1aa416a31738f856c0e
Bennie18016 <98323950+Bennie18016@users.noreply.github.com>
2022-11-09 23:18:38 +0000

    Interaction Finished
    Now you can:
    Pick doors open which shows progress and animates once finished
    Easy to add other functions (Windows, Pagers, Money etc)

--- Unity/Assets/Scripts/Enviroment/Interaction.cs
+++ Unity/Assets/Scripts/Enviroment/Interaction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using Player;
 
@@ -16,6 +17,11 @@ namespace Enviroment
         [Tooltip("How long to hold it")]
         public int time;
 
+        public enum mode
+        { Door, Pager }
+        [Tooltip("What it does: door, lockpick, pager etc")]
+        public mode Modes;
+
         [Header("Fundamentals")]
         [Tooltip("The player gameobject")]
         public GameObject player;
@@ -25,54 +31,75 @@ namespace Enviroment
         public TMP_Text textOBJ;
         [Tooltip("String for input text")]
         public string text;
+        [Tooltip("Image of interaction circle")]
+        public Image interactionCircle;
 
         float _time;
-        bool holding;
+        float _fillTime;
+        float _fillMultipler;
+        bool _holding;
 
         private void Start()
         {
             textOBJ.gameObject.transform.name = string.Format("{0}'s text OBJ", gameObject.transform.name);
+            _fillMultipler = time * 0.01f;
+            _fillMultipler *= 4;
         }
 
         private void Update()
         {
             float distance = Vector3.Distance(player.transform.position, transform.position);
-            if(distance < dist)
+            if (distance < dist)
             {
                 textOBJ.enabled = true;
                 textOBJ.text = text;
-                if(hold) { holding = Input.GetKey(key); }
-                else if(Input.GetKeyDown(key))
+                if (hold) { _holding = Input.GetKey(key); }
+                else if (Input.GetKeyDown(key))
                 {
                     Finish();
                 }
-            } else 
+            }
+            else
             {
                 textOBJ.enabled = false;
             }
 
-            if(holding)
+            if (_holding)
             {
-                player.GetComponent<Movement>().canMove = !holding;
+                player.GetComponent<Movement>().canMove = !_holding;
+            }
+            else
+            {
+                player.GetComponent<Movement>().canMove = !_holding;
+                _fillTime = 0;
             }
 
+            interactionCircle.fillAmount = _fillTime;
             TimeManagement();
         }
 
         private void TimeManagement()
         {
-            if(_time < time && !holding) { _time = 0; }
-            if(_time > time) { Finish(); }
+            if (_time < time && !_holding) { _time = 0; }
+            if (_time > time) { Finish(); }
 
+            if (_holding) { _fillTime += _fillMultipler * Time.deltaTime; }
             _time += 1 * Time.deltaTime;
         }
 
         public void Finish()
         {
             Debug.Log("Succesfully Finished!");
-            this.enabled = false;
+            player.GetComponent<Movement>().canMove = true;
             Destroy(textOBJ.gameObject);
-            _time = 0;
+            interactionCircle.enabled = false;
+
+            if ((int)Modes == 0)
+            {
+                GetComponent<Animation>().Play();
+            }
+
+            this.enabled = false;
         }
     }
 }
