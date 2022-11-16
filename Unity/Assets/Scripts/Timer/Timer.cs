53dda0f42e450f11045f43d3c83f4eb8f019369a
Bonnie Jenkins <18016a@totton.ac.uk>
2022-11-16 11:17:31 +0000

    Fixed timer
    Timer now shows with a 0 in-front of a number if its less than 10
    (01:04 not 1:4)

--- Unity/Assets/Scripts/Timer/Timer.cs
+++ Unity/Assets/Scripts/Timer/Timer.cs
@@ -10,11 +10,27 @@ public class Timer : MonoBehaviour
 
     private void Update()
     {
-        string text = string.Format("{0}:{1}", minutes, (int)seconds);
-        timer.text = text;
+        string _secs = ((int)seconds).ToString();
+        string _mins = minutes.ToString();
 
-
-        if(seconds >= 60) { minutes += 1; seconds = 0; }
+        if (seconds >= 60) { minutes += 1; seconds = 0; }
         seconds += 1 * Time.deltaTime;
+
+        string text;
+        if(minutes > 10 && seconds > 10)
+        {
+           text = string.Format("{0}:{1}", _mins, _secs);
+        } else if (minutes > 10)
+        {
+           text = string.Format("{0}:0{1}", _mins, _secs);
+        } else if (seconds > 10)
+        {
+            text = string.Format("0{0}:{1}", _mins, _secs);
+        }
+        else
+        {
+            text = string.Format("0{0}:0{1}", _mins, _secs);
+        }
+        timer.text = text;
     }
 }
