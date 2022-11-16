3456957c11e18ebff8491693b360f547e215006d
Bonnie Jenkins <18016a@totton.ac.uk>
2022-11-16 13:54:11 +0000

    Cameras
    Started Cameras

--- Unity/Assets/Scripts/A.I/Dead.cs
+++ Unity/Assets/Scripts/A.I/Dead.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Dead : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Fundamentals")]
+    [Tooltip("Is this a camera?")]
+    public bool Camera;
+    [Tooltip("Where must they walk to investigate")]
+    public Transform InvestergationArea;
 
-    // Update is called once per frame
-    void Update()
+    private GameObject AI;
+
+    private void Start()
     {
-        
+        List<GameObject> Guards = new List<GameObject>();
+
+        foreach(GameObject guard in FindGameObjectsWithTag("AI"))
+        {
+            if(guard.name.ToLower() == "guard")
+            {
+                Guards.Add(guard);
+            }
+        }
     }
 }
